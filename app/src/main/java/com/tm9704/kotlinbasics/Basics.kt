cf2a263655b30061490f3b2c6eeeace15d7aa2a3
tm9704 <tm9704@korea.ac.kr>
2022-08-06 16:55:27 +0900

    Kotlin : Variable var&val

--- app/src/main/java/com/tm9704/kotlinbasics/Basics.kt
+++ app/src/main/java/com/tm9704/kotlinbasics/Basics.kt
@@ -1,5 +1,12 @@
 package com.tm9704.kotlinbasics
 
 fun main(){
+    var myName = "Denis"
+    myName = "Heidi"
+    print("Hello " + myName)
 
+    val myName2 = "Frank"
+//    myName2 = "Min" //error
 }
+
+
