4375bd27d775a624c962724a4aa3a42b3996db55
tm9704 <tm9704@korea.ac.kr>
2022-08-07 14:32:44 +0900

    Kotlin: While Loop

---
+++ app/src/main/java/com/tm9704/kotlinbasics/Loop.kt
@@ -0,0 +1,11 @@
+package com.tm9704.kotlinbasics
+
+fun main(){
+    // While loop executes a block of code repeatedly as long as a given condition is true
+    var x = 1
+    while(x<=10){
+        println("$x")
+        x++
+    }
+    println("while loop is done")
+}
