f23c19d6304a574b1aac30f12e0cbb8de1c1ba4c
tm9704 <tm9704@korea.ac.kr>
2022-08-07 14:36:12 +0900

    Kotlin: Do While Loop

--- app/src/main/java/com/tm9704/kotlinbasics/Loop.kt
+++ app/src/main/java/com/tm9704/kotlinbasics/Loop.kt
@@ -3,9 +3,16 @@ package com.tm9704.kotlinbasics
 fun main(){
     // While loop executes a block of code repeatedly as long as a given condition is true
     var x = 1
-    while(x<=10){
+    /*while(x<=10){
         println("$x")
         x++
     }
-    println("while loop is done")
+    println("while loop is done")*/
+    x = 15
+    do{
+        println("$x")
+        x++
+    }while(x <= 10)
+    println("do while loop is done")
+
 }
