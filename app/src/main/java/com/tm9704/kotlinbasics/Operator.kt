d7e2d8490ab8cbfb26872a05d56bbaf35787a95c
tm9704 <tm9704@korea.ac.kr>
2022-08-07 12:54:44 +0900

    Kotlin: Arithmetic Operators

---
+++ app/src/main/java/com/tm9704/kotlinbasics/Operator.kt
@@ -0,0 +1,17 @@
+package com.tm9704.kotlinbasics
+
+fun main(){
+    // Arithmetic opertors (+, -, *, /, %)
+    var result = 5+3
+    result /= 2
+    result *= 2
+    result += 2
+    result -= 2
+    result = 15 % 2
+
+    val a = 5.0
+    val b = 3.0
+    var resultDouble : Double
+    resultDouble = a / b
+    print(resultDouble)
+}
