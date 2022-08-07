d409dd9bccecccf5f1ce3b511fc062be83f3c0d5
tm9704 <tm9704@korea.ac.kr>
2022-08-07 12:33:16 +0900

    Kotlin: String Template

---
+++ app/src/main/java/com/tm9704/kotlinbasics/StringTemplate.kt
@@ -0,0 +1,10 @@
+package com.tm9704.kotlinbasics
+
+fun main(){
+    //Strings
+    val myStr = "Hello World"
+    var firstCharInStr = myStr[0]
+    var lastCharInStr = myStr[myStr.length - 1]
+
+    print("Frist character $firstCharInStr and the length of myStr is ${myStr.length}")
+}
