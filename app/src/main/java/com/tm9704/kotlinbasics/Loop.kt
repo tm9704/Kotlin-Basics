aec406a2e69e008f71415f373cb3d608bb23d913
tm9704 <tm9704@korea.ac.kr>
2022-08-10 13:55:50 +0900

    Kotlin: Loop Practice

--- app/src/main/java/com/tm9704/kotlinbasics/Loop.kt
+++ app/src/main/java/com/tm9704/kotlinbasics/Loop.kt
@@ -2,7 +2,7 @@ package com.tm9704.kotlinbasics
 
 fun main(){
     // While loop executes a block of code repeatedly as long as a given condition is true
-    var x = 100
+    var x = 12
     /*while(x<=10){
         println("$x")
         x++
@@ -24,7 +24,7 @@ fun main(){
             feltTemp = "comfy"
             println("it's comfy now")
         }
-    }*/
+    }
 
     //for loop
     for(num in 1..10){
@@ -41,5 +41,22 @@ fun main(){
 
     for(i in 10 downTo 1 step 2){ // Same as - for(i in 10.downTo(1).step(2)
         print("$i ")
+    }*/
+
+    // Loop Practice
+    for(num in 1..10000){
+        if(num == 9001)
+            print("IT'S OVER 9000!")
+    }
+
+    var humidity = "humid"
+    var humidityLevel = 80
+    while(humidity == "humid"){
+        humidityLevel -= 5
+        println("humidity decreased")
+        if(humidityLevel < 60){
+            humidity = "comfy"
+            println("it's comfy now")
+        }
     }
 }
